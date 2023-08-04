aaa67693fef45571eaf04e951c850d6a7b00b782
Arpit Tyagi <arpittyagi102@gmail.com>
2023-08-04 15:08:35 +0530

    Q.1 part-2 [done]

--- src/component/list/List.jsx
+++ src/component/list/List.jsx
@@ -6,7 +6,7 @@ import ListHeaderCell from "./ListHeaderCell";
 
 import styles from "./List.module.css";
 
-const List = ({ rows }) => {
+const List = ({ rows, timestamps}) => {
   return (
     <table className={styles.container}>
       <thead>
@@ -24,7 +24,11 @@ const List = ({ rows }) => {
             <ListRowCell>{row["&id"]}</ListRowCell>
             <ListRowCell>{row.executionDetails.buySellIndicator}</ListRowCell>
             <ListRowCell>{row.executionDetails.orderStatus}</ListRowCell>
-            <ListRowCell>{row.orderSubmitted}</ListRowCell>
+            <ListRowCell>{
+                timestamps.results.find(                      
+                    (item) => item['&id'] == row['&id']
+                    ).timestamps.orderSubmitted
+                }</ListRowCell>
             <ListRowCell>{row.bestExecutionData.orderVolume.USD}</ListRowCell>
           </ListRow>
         ))}
